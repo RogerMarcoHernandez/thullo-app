f7086c099bb6f3e64de624e071e23ec301991205
RogerMarcoHernandez <bluntun@gmail.com>
2023-09-09 14:22:35 +0200

    feat: footer socials and contact, and privacy page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
-import "./globals.css";
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
+import "./globals.css";
 import Providers from "./providers";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { Link } from "@nextui-org/react";
+import Image from "next/image";
 import { default as NextLink } from "next/link";
+import { FaGithub } from "react-icons/fa";
 
 const Footer = () => (
   <footer className="bg-gray-900 text-white py-6">
@@ -18,7 +20,53 @@ const Footer = () => (
         </Link>
       </div>
       {/* Social media icons */}
+      <div className="bg-white rounded-lg shadow-md p-6 mt-2">
+        <NextLink
+          href="https://github.com/RogerMarcoHernandez/thullo-app"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-primary hover:text-primary-dark transition duration-300"
+        >
+          <FaGithub name="github" size={24} className="mb-4" />
+          <h3 className="text-lg font-semibold mb-2">GitHub</h3>
+          <p className="text-gray-600">Check out our code on GitHub.</p>
+        </NextLink>
+      </div>
+      <div className="bg-white rounded-lg shadow-md p-6 mt-2">
+        <NextLink
+          href="https://devchallenges.io/portfolio/RogerMarcoHernandez"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-primary hover:text-primary-dark transition duration-300"
+        >
+          <Image
+            src="/devchallenges.png"
+            alt="DevChallenges"
+            height={24}
+            width={24}
+            className="mb-4"
+          />
+          <h3 className="text-lg font-semibold mb-2">DevChallenges</h3>
+          <p className="text-gray-600">View my portfolio on DevChallenges.</p>
+        </NextLink>
+      </div>
       {/* Contact information */}
+      <div className="bg-white rounded-lg shadow-md p-6 mt-2">
+        <h3 className="text-lg font-semibold mb-2">Contact Information</h3>
+        <p className="text-gray-600">
+          Have questions or feedback? Feel free to reach out!
+        </p>
+        <div className="mt-4">
+          <p className="font-semibold">Email:</p>
+          <Link
+            as={NextLink}
+            href="mailto:bluntun@gmail.com"
+            className="text-primary hover:underline transition duration-300"
+          >
+            bluntun@gmail.com
+          </Link>
+        </div>
+      </div>
     </div>
   </footer>
 );
