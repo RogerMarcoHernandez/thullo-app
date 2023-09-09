0a64fa56a9e0712c13f822d3bbb277b429972a12
RogerMarcoHernandez <bluntun@gmail.com>
2023-09-09 13:44:29 +0200

    chore: set up project with design deps

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import Providers from "./providers";
+
+const montserrat = Montserrat({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Thullo - Collaborative Task Management",
+  description:
+    "Thullo is a powerful collaborative task management application inspired by Trello. Organize your projects, manage tasks, and work seamlessly with your team.",
+};
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang="en" className="dark">
+      <body className={montserrat.className}>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
